62828b41a5cac0cd5b01e91589c7d80e54e311a2
Noël Ruault <noel@cast.ai>
2022-06-27 00:05:47 +0200

    tidy up

--- setup.sh
+++ setup.sh
@@ -21,9 +21,20 @@ cd && git clone https://github.com/noelruault/config.git
 ln -sF $HOME/config $HOME/.config
 ln -sF $HOME/config/zshrc-config $HOME/.zshrc-config
 ln -sF $HOME/config/zshrc-config/zshrc $HOME/.zshrc
+ln -sF $HOME/config/code-editors/vim/vimrc $HOME/.vimrc
+
+### SET PROFILE ###
+# Many system-wide settings including PATH are set in /etc/profile
+# source /etc/profile # System-wide .profile for sh(1)
+
+# .zshrc is not the right place to set $PATH or any other environment variable.
+# Environment variables should be set in ~/.zprofile
+ln -sF $HOME/config/zshrc-config/zprofile $HOME/.zprofile
+
 
 brew install coreutils # gdate
 brew install fzf # fzf
+# Troubleshooting: https://spaceship-prompt.sh/troubleshooting/
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1 && ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme" # zsh theme: Spaceship
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions  # zsh-autosuggestionszsh-autosuggestions
@@ -33,3 +44,6 @@ touch ~/.config/zshrc-config/aliases/private/secrets && chmod +x ~/.config/zshrc
 
 # Clean directories
 cd && rm -rf "~/zsh-autosuggestions ~/zsh-syntax-highlighting"
+
+# TODO install brew
+# TODO install fonts
