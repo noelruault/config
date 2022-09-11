823f091ceccdc7949aed04376de8428d334beb13
Noël Ruault <noelruault@Noels-MacBook-Pro.local>
2022-09-11 18:31:01 +0200

    Not setting gdate from zprofile

--- zshrc/aliases/nruault/assets/noeliodomain.sh
+++ zshrc/aliases/nruault/assets/noeliodomain.sh
@@ -4,6 +4,7 @@ if nc -zw1 google.com 443 &>/dev/null; then echo; else exit 1; fi
 domain_status=$(whois noel.io | grep -m1 "Domain Status")
 domain_expiry_date=$(whois noel.io | grep "Registry Expiry Date" | grep -oE '(\d{4})-(\d\d)-(\d\d)' | sed 's/[\._-]//g')
 
+gdate=$HOMEBREW_PREFIX/bin/gdate
 current_epoch_time=$(gdate +%s)
 domain_expity_date_epoch=$(gdate --date=$domain_expiry_date +%s)
 days_until_expiration=$(((domain_expity_date_epoch - current_epoch_time) / 86400))
